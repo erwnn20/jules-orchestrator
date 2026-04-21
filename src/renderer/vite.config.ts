8a1a16318ba6a7e6f3250f6225bbb90da8418721
verwann <erwann.varlet@ynov.com>
2026-04-21 11:19:58 +0200

    update dev and build scripts, add path aliases

--- src/renderer/vite.config.ts
+++ src/renderer/vite.config.ts
@@ -1,12 +1,20 @@
-﻿import { defineConfig } from 'vite'
+﻿import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-    // root: 'src/renderer',
-    plugins: [react()],
-    base: './',
-    build: {
-        outDir: 'dist',
-        emptyOutDir: true,
+  // root: 'src/renderer',
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@pages': path.resolve(__dirname, './components/pages'),
+      '@components': path.resolve(__dirname, './components'),
+      '@interfaces': path.resolve(__dirname, './interfaces'),
     },
+  },
 })
