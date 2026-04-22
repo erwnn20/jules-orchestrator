9f96464224a9f461ad8e20a7675b3bffb3eea8df
verwann <erwann.varlet@ynov.com>
2026-04-22 17:30:20 +0200

    integrate TailwindCSS: update dependencies, configure Vite, and add initial styling

--- src/renderer/vite.config.ts
+++ src/renderer/vite.config.ts
@@ -1,10 +1,11 @@
-﻿import {defineConfig} from 'vite'
+﻿import tailwindcss from "@tailwindcss/vite";
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   // root: 'src/renderer',
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '.',
   build: {
     outDir: 'dist',
