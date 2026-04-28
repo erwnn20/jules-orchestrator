bbbaa8aacf075ef1027f63b99b88d155d49a68c6
verwann <erwann.varlet@ynov.com>
2026-04-28 23:22:47 +0200

    refactor(basePage): update title and subtitle props to support ReactNode or string

--- src/renderer/components/pages/base.page.tsx
+++ src/renderer/components/pages/base.page.tsx
@@ -3,20 +3,20 @@ import { ReactNode } from "react";
 
 export default function BasePage({ children, title, subtitle }: {
   children: ReactNode,
-  title: string,
+  title: ReactNode | string,
   subtitle?: ReactNode | string
 }) {
   return (
     <div className='px-10 py-8'>
-      <h1 className='text-title text-primary-foreground mb-1 font-semibold'>
-        {title}
-      </h1>
-      {
-        subtitle && typeof subtitle === 'string' ?
-          <p className='text-subtitle text-faint mb-8'>{subtitle}</p>
-          : subtitle
-      }
-      
+      {typeof title === 'string' ?
+        <h1 className='text-title text-primary-foreground mb-1 font-semibold'>
+          {title}
+        </h1>
+        : title}
+      {subtitle && typeof subtitle === 'string' ?
+        <p className='text-subtitle text-faint mb-8'>{subtitle}</p>
+        : subtitle}
+
       {children}
 
     </div>
