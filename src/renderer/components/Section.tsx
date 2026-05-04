5dec4ddc9ea1d52f532c644582307e1fd5627e36
verwann <erwann.varlet@ynov.com>
2026-05-04 09:04:18 +0200

    refactor(Section): support optional `addon` element for flexible header layout

--- src/renderer/components/Section.tsx
+++ src/renderer/components/Section.tsx
@@ -1,11 +1,18 @@
 ﻿import { ReactNode } from "react";
 
 
-export default function Section({ title, children }: { title: string, children: ReactNode }) {
+export default function Section({ title, addon, children }: {
+  title: string,
+  addon?: ReactNode,
+  children: ReactNode
+}) {
   return (
     <section className='mb-8'>
-      <div className='mb-3 text-meta text-faint uppercase tracking-wider'>
-        {title}
+      <div className='mb-3 flex items-baseline justify-between'>
+        <div className='text-meta text-faint uppercase tracking-wider'>
+          {title}
+        </div>
+        {addon}
       </div>
 
       {children}
