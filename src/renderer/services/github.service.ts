8386fcc15c1b08979f4c7df0f67a6c7d2c48950b
verwann <erwann.varlet@ynov.com>
2026-05-06 22:56:47 +0200

    feat: add GithubService to handle GitHub repository methods

---
+++ src/renderer/services/github.service.ts
@@ -0,0 +1,13 @@
+﻿import {
+  GetRepositoryRequest,
+  ListRepositoryRequest
+} from "@github/repositories/repository.interfaces";
+import { Repository } from "@github/repositories/repository.model";
+
+
+export abstract class GithubService {
+
+  static getRepo: (args: GetRepositoryRequest) => Promise<Repository> = window.api.github.repository.get;
+  static getRepos: (args: ListRepositoryRequest) => Promise<Repository[]> = window.api.github.repository.list;
+
+}
