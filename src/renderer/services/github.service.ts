4bb174a0f7e5149e8daee8af652c0e68b9298a1f
verwann <erwann.varlet@ynov.com>
2026-05-16 17:54:10 +0200

    feat: add `getIssuesPR` method to GithubService for listing GitHub pull requests by issues

--- src/renderer/services/github.service.ts
+++ src/renderer/services/github.service.ts
@@ -1,4 +1,5 @@
-﻿import {
+﻿import { ListIssuesRequest, ListIssuesResponse } from "@github/pr/pr.interfaces";
+import {
   GetRepositoryRequest,
   ListRepositoryRequest
 } from "@github/repositories/repository.interfaces";
@@ -9,5 +10,9 @@ export abstract class GithubService {
 
   static getRepo: (args: GetRepositoryRequest) => Promise<Repository> = window.api.github.repository.get;
   static getRepos: (args: ListRepositoryRequest) => Promise<Repository[]> = window.api.github.repository.list;
+
+  //
+
+  static getIssuesPR: (args: ListIssuesRequest) => Promise<ListIssuesResponse> = window.api.github.pr.list;
 
 }
