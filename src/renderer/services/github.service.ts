4eb2f9b1cfc04cbeda96959afbe26b05e2ff2d06
verwann <erwann.varlet@ynov.com>
2026-05-17 12:04:06 +0200

    style: clean up comments in `github.service.ts` for consistency

--- src/renderer/services/github.service.ts
+++ src/renderer/services/github.service.ts
@@ -16,9 +16,9 @@ export abstract class GithubService {
   static getRepo: (args: GetRepositoryRequest) => Promise<Repository> = window.api.github.repository.get;
   static getRepos: (args: ListRepositoryRequest) => Promise<Repository[]> = window.api.github.repository.list;
 
-  static getRepoPRs: (args: ListPRRequest) => Promise<ListPRResponse> = window.api.github.repository.pr.list;
-
   //
+
+  static getRepoPRs: (args: ListPRRequest) => Promise<ListPRResponse> = window.api.github.repository.pr.list;
 
   static getIssuesPR: (args: ListIssuesRequest) => Promise<ListIssuesResponse> = window.api.github.pr.list;
 
