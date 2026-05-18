070746661dd101d67ee18399b50fe52ccb77dcb1
verwann <erwann.varlet@ynov.com>
2026-05-18 23:51:40 +0200

    feat: introduce repository and branch hooks using React Query for GitHub data integration

---
+++ src/renderer/hooks/github/repositories.hooks.ts
@@ -0,0 +1,29 @@
+﻿import { ListBranchesRequest } from "@github/branch/branch.interfaces";
+import {
+  GetRepositoryRequest,
+  ListRepositoryRequest
+} from "@github/repositories/repository.interfaces";
+import { GithubService } from "@renderer/data/github.service";
+import { useQuery } from "@tanstack/react-query";
+
+
+export function useRepository({ owner, repo }: GetRepositoryRequest) {
+  return useQuery({
+    queryKey: ['repositories', owner, repo],
+    queryFn: () => GithubService.getRepo({ owner, repo })
+  })
+}
+
+export function useRepositories(args: ListRepositoryRequest) {
+  return useQuery({
+    queryKey: ['repositories', args],
+    queryFn: () => GithubService.getRepos(args)
+  })
+}
+
+export function useRepoBranches({ owner, repo,...args }: ListBranchesRequest) {
+  return useQuery({
+    queryKey: ['repositories', owner, repo, 'branches', args],
+    queryFn: () => GithubService.getRepoBranches({ owner, repo,...args })
+  })
+}
