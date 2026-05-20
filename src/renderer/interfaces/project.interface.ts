6119c802571b2cccaaf19be050a0769ccaf17dc9
verwann <erwann.varlet@ynov.com>
2026-05-20 10:44:10 +0200

    refactor: add `branches` getter in `Project` for sorted branch data and extend `ProjectBranch` interface

--- src/renderer/interfaces/project.interface.ts
+++ src/renderer/interfaces/project.interface.ts
@@ -1,9 +1,12 @@
-﻿import { PullRequest } from "@github/pr/pr.model";
+﻿import { Branch } from "@github/branch/branch.interfaces";
+import { PullRequest } from "@github/pr/pr.model";
 import { Repository } from "@github/repositories/repository.model";
 import { Source } from "@jules/sources/source.model";
+import { useRepoBranches } from "@renderer/hooks/github/repositories.hooks";
 import { useSessionsBySource } from "@renderer/hooks/jules/sources.hooks";
 import { Agent } from "@renderer/interfaces/agent.interface";
 import { PullRequest as IPullRequest } from "@renderer/interfaces/pullRequest.interface";
+import { UseQueryResult } from "@tanstack/react-query";
 
 
 export interface IProject {
@@ -41,6 +44,28 @@ export class ProjectOptionalRepo {
     return agents.filter(({ state }) => true /*ACTIVE_STATES.includes(state)*/)
   }
 
+  get branches(): UseQueryResult<ProjectBranch[]> {
+    const query = useRepoBranches({
+      repo: this.repository?.name ?? '',
+      owner: this.repository?.owner.login ?? ''
+    })
+
+    if (!query.data || !this.repository) return query
+
+    const sorted = [...query.data]
+    .map((branch: Branch): ProjectBranch => ({
+      ...branch,
+      isDefault: branch.name === this.repository?.defaultBranch
+    }))
+    .sort((a, b) => {
+      if (a.isDefault) return -1
+      if (b.isDefault) return 1
+      return 0 // TODO fix desired order
+    })
+
+    return { ...query, data: sorted }
+  }
+
   get prs(): {
     data?: PullRequest[],
     isLoading: boolean,
@@ -55,3 +80,5 @@ export class Project extends ProjectOptionalRepo {
     super(repository, sources)
   }
 }
+
+export interface ProjectBranch extends Branch {isDefault?: boolean}
