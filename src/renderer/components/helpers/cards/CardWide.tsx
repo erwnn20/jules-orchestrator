fabdb082a02d9e79ea0e7d4d497ba258d8b24cd0
verwann <erwann.varlet@ynov.com>
2026-05-21 16:09:04 +0200

    refactor(CardWide): add optional `id` prop for increased flexibility in component usage

--- src/renderer/components/helpers/cards/CardWide.tsx
+++ src/renderer/components/helpers/cards/CardWide.tsx
@@ -1,8 +1,12 @@
 ﻿import { ReactNode } from "react";
 
-export default function CardWide({ children, className }: { children: ReactNode, className?: string }) {
+export default function CardWide({ children, id, className }: {
+  children: ReactNode,
+  id?: string,
+  className?: string
+}) {
   return (
-    <div className={
+    <div id={id} className={
       'flex items-center gap-3 px-4 py-3 ' +
       'bg-panel ' +
       'border border-border-color rounded-lg' +
