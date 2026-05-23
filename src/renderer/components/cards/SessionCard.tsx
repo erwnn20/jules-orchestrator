6ee29f18710207e67d739b5bbb9a6ef83ed0d191
verwann <erwann.varlet@ynov.com>
2026-05-23 23:46:02 +0200

    feat(SessionCard): introduce reusable card component for session details rendering

---
+++ src/renderer/components/cards/SessionCard.tsx
@@ -0,0 +1,35 @@
+﻿import CardWide from "@components/helpers/cards/CardWide";
+import SessionStatusDot from "@components/helpers/dots/SessionStatusDot";
+import { Session } from "@jules/sessions/session.model";
+import { ReactNode } from "react";
+
+
+export function SessionCard({ session, id, className = '', contents: { title, subtitle, end } }: {
+  session: Session,
+  contents: {
+    title: ReactNode,
+    subtitle?: ReactNode,
+    end?: ReactNode
+  },
+  id?: string,
+  className?: string
+}) {
+  return (
+    <CardWide id={id} className={className}>
+      <SessionStatusDot session={session}/>
+      <div className="flex-1">
+        <div className="mb-1 text-subtitle text-primary-foreground">
+          {title}
+        </div>
+        {subtitle && (
+          <div className="text-meta text-muted text-ellipsis">
+            {subtitle}
+          </div>)}
+      </div>
+      {end && (
+        <div className='text-label text-faint'>
+          {end}
+        </div>)}
+    </CardWide>
+  )
+}
