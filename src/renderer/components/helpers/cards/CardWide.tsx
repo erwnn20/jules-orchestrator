e543e01be6855ae4e7c0d9cfa2ba2a6cc7085658
verwann <erwann.varlet@ynov.com>
2026-05-25 19:32:30 +0200

    refactor(CardWide): extend props to support title, subtitle, start, and end render slots

--- src/renderer/components/helpers/cards/CardWide.tsx
+++ src/renderer/components/helpers/cards/CardWide.tsx
@@ -1,11 +1,10 @@
 ﻿import { twMerge } from '@renderer/utils/tw.utils';
 import { ReactNode } from "react";
 
-export default function CardWide({ children, id, className }: {
-  children: ReactNode,
-  id?: string,
-  className?: string
-}) {
+export default function CardWide({ id, className = '', ...args }: {
+  id?: string, className?: string
+} & ({ children: ReactNode }
+  | { title: ReactNode, subtitle?: ReactNode, start?: ReactNode, end?: ReactNode })) {
   return (
     <div id={id} className={twMerge(
       'flex items-center gap-3 px-4 py-3',
@@ -14,7 +13,22 @@ export default function CardWide({ children, id, className }: {
       'transition-colors duration-350',
       className
     )}>
-      {children}
+      {'children' in args ? args.children : (<>
+        {args.start && args.start}
+        <div className="flex-1">
+          <div className="mb-1 text-subtitle text-primary-foreground font-medium">
+            {args.title}
+          </div>
+          {args.subtitle && (
+            <div className="text-meta text-muted text-ellipsis">
+              {args.subtitle}
+            </div>)}
+        </div>
+        {args.end && (
+          <div className='text-label text-faint'>
+            {args.end}
+          </div>)}
+      </>)}
     </div>
   )
 }
