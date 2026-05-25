4d3f7361d642938ecba86f8613f966f40b619b85
verwann <erwann.varlet@ynov.com>
2026-05-25 19:33:04 +0200

    refactor(PullRequestCard): simplify component structure by leveraging `CardWide` render slots

--- src/renderer/components/cards/PullRequestCard.tsx
+++ src/renderer/components/cards/PullRequestCard.tsx
@@ -20,22 +20,20 @@ function PullRequestCardWide({ pr, className = '' }: {
   pr: PullRequest | PullRequestList,
   className?: string
 }) {
-  return (
-    <CardWide className={className}>
-      <PullRequestDot state={pr.state}/>
-      <div className='flex-1'>
-        <div className='mb-1 flex items-center gap-1'>
-          <span className={'text-subtitle text-primary-foreground font-medium'}>{pr.title}</span>
-          <span className="text-ghost">·</span>
-          <span className={'text-label text-muted'}>{pr.updatedAt.toLocaleString('fr-FR')}</span>
-        </div>
-        <div className='flex items-center gap-1 text-meta text-muted'>
-          <GitBranch className='h-3 w-3'/> {pr.head.ref}
-          <span className="mx-0.5 text-ghost">→</span>
-          <GitMerge className='h-3 w-3'/> {pr.base.ref}
-        </div>
-      </div>
-      <Link to={pr.htmlUrl} text='voir PR'/>
-    </CardWide>
-  )
+  return (<CardWide
+    className={className}
+    start={<PullRequestDot state={pr.state}/>}
+    title={(
+      <div className='flex items-center gap-1'>
+        {pr.title} <span className="text-ghost">·</span>
+        <span className={'text-label text-muted'}>{pr.updatedAt.toLocaleString('fr-FR')}</span>
+      </div>)}
+    subtitle={(
+      <div className='flex items-center gap-1'>
+        <GitBranch className='h-3 w-3'/> {pr.head.ref}
+        <span className="mx-0.5 text-ghost">→</span>
+        <GitMerge className='h-3 w-3'/> {pr.base.ref}
+      </div>)}
+    end={<Link to={pr.htmlUrl} text='voir PR'/>}
+  />)
 }
