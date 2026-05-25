2be61c921ebea8e324c97b6b7fac9c0c60dc6055
verwann <erwann.varlet@ynov.com>
2026-05-25 12:26:42 +0200

    refactor(PullRequestCard): enhance layout with updated metadata display and icon additions

--- src/renderer/components/cards/PullRequestCard.tsx
+++ src/renderer/components/cards/PullRequestCard.tsx
@@ -3,7 +3,7 @@ import PullRequestDot from "@components/helpers/dots/PullRequestDot";
 import Link from "@components/helpers/Link";
 import { PullRequestList } from "@github/pr/list.model";
 import { PullRequest } from "@github/pr/pr.model";
-import { GitBranch } from "lucide-react";
+import { GitBranch, GitMerge } from "lucide-react";
 
 
 export function PullRequestCard({ pr, style = 'wide', className = '' }: {
@@ -24,13 +24,16 @@ function PullRequestCardWide({ pr, className = '' }: {
     <CardWide className={className}>
       <PullRequestDot state={pr.state}/>
       <div className='flex-1'>
-          <span className='mb-1 text-subtitle text-primary-foreground font-medium'>
-            {pr.title}
-          </span>
-        <span className='flex items-center gap-1 text-label text-muted'>
-          <GitBranch className='h-3 w-3'/>
-          {pr.head.ref} · {pr.base.ref /*TODO change by creation/update date*/}
-        </span>
+        <div className='mb-1 flex items-center gap-1'>
+          <span className={'text-subtitle text-primary-foreground font-medium'}>{pr.title}</span>
+          <span className="text-ghost">·</span>
+          <span className={'text-label text-muted'}>{pr.updatedAt.toLocaleString('fr-FR')}</span>
+        </div>
+        <div className='flex items-center gap-1 text-meta text-muted'>
+          <GitBranch className='h-3 w-3'/> {pr.head.ref}
+          <span className="mx-0.5 text-ghost">→</span>
+          <GitMerge className='h-3 w-3'/> {pr.base.ref}
+        </div>
       </div>
       <Link to={pr.htmlUrl} text='voir PR'/>
     </CardWide>
