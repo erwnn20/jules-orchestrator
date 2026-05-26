03fb2dd18a6cf21e9068cde34b0568a1688e8573
verwann <erwann.varlet@ynov.com>
2026-05-26 16:20:02 +0200

    feat(ErrorCard): add `default` style and improve error rendering logic

--- src/renderer/components/cards/ErrorCard.tsx
+++ src/renderer/components/cards/ErrorCard.tsx
@@ -1,5 +1,6 @@
 ﻿import CardWide from "@components/helpers/cards/CardWide";
 import { ApiError } from "@renderer/utils/ipc-error.utils";
+import { twMerge } from "@renderer/utils/tw.utils";
 import { TriangleAlert } from "lucide-react";
 
 
@@ -8,14 +9,39 @@ type Error = ApiError | {
   message: string,
 }
 
-  style?: 'wide',
 export default function ErrorCard({ error, style, className = '' }: {
   error: Error,
+  style: 'default' | 'wide',
   className?: string,
 }) {
-  return (
-    <ErrorCardWide error={error} className={className}/>
-  )
+  switch (style) {
+    case 'wide':
+      return (<ErrorCardWide error={error} className={className}/>)
+    case 'default':
+    default:
+      return (<ErrorCardDefault error={error} className={className}/>)
+  }
+}
+
+function ErrorCardDefault({ error, className = '' }: {
+  error: Error,
+  className?: string
+}) {
+  const title = 'code' in error ? error.code : error.name
+
+  return (<div className={twMerge(
+    'flex flex-col gap-2 p-5',
+    'bg-panel', 'border border-border-color rounded-lg',
+    'transition-colors duration-350',
+  )}>
+    <p className={'flex items-center gap-1 text-base text-accent-red'}>
+      <TriangleAlert className="w-4 h-4 mr-1"/>
+      Error {title !== 'Error' && `: ${error.name}`}
+    </p>
+    <p className={'text-base text-faint text-ellipsis overflow-hidden'}>
+      {error.message}
+    </p>
+  </div>)
 }
 
 function ErrorCardWide({ error, className = '' }: {
