86f37d99e31e4d5e46b9a0ee2a81f7f74756e1d0
verwann <erwann.varlet@ynov.com>
2026-05-26 12:03:19 +0200

    refactor(BaseController): enhance error handling with normalized error responses

--- src/electron/controllers/base.controller.ts
+++ src/electron/controllers/base.controller.ts
@@ -1,4 +1,5 @@
-﻿import { ipcMain } from "electron";
+﻿import { normalizeError } from "@electron/utils/error.util";
+import { ipcMain } from "electron";
 
 export abstract class BaseController<Client> {
   protected constructor(
@@ -10,6 +11,13 @@ export abstract class BaseController<Client> {
   ) { }
 
   registerHandlers() {
-    for (const { channel, listener } of this.handlers) ipcMain.handle(channel, listener)
+    for (const { channel, listener } of this.handlers)
+      ipcMain.handle(channel, async (event, ...args) => {
+        try {
+          return { ok: true, data: await listener(event, ...args) }
+        } catch (err) {
+          return { ok: false, error: normalizeError(err) }
+        }
+      })
   }
 }
